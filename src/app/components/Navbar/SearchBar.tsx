5668b14943f647f1a401253eab80af510b4e27a9
Malvin A <malvinaudriansyah77@gmail.com>
2025-01-20 14:34:23 +0700

    Can use enter key for initiate search besides clicking on search button

--- src/app/components/Navbar/SearchBar.tsx
+++ src/app/components/Navbar/SearchBar.tsx
@@ -6,10 +6,23 @@ import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
 const SearchBar = () => {
   const searchRef = useRef<HTMLInputElement>(null);
   const router = useRouter();
-  const handleSearchInput = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    const keyword = searchRef.current?.value;
-    router.push(`/search/${keyword}`);
+  const handleSearchInput = (
+    event:
+      | React.MouseEvent<HTMLButtonElement>
+      | React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (event.type === `keydown`) {
+      const keyboardEvent = event as React.KeyboardEvent<HTMLInputElement>;
+      if (keyboardEvent.key === `Enter`) {
+        event.preventDefault();
+        const keyword = searchRef.current?.value;
+        router.push(`/search/${keyword}`);
+      }
+    } else if (event.type === `click`) {
+      event.preventDefault();
+      const keyword = searchRef.current?.value;
+      router.push(`/search/${keyword}`);
+    }
   };
   return (
     <div className="relative w-full md:w-[250px]">
@@ -17,6 +30,7 @@ const SearchBar = () => {
         className="w-full p-3 rounded-md border-solid border-[1px] border-black"
         placeholder="search anime..."
         ref={searchRef}
+        onKeyDown={handleSearchInput}
       />
       <button className="absolute top-3 end-3" onClick={handleSearchInput}>
         <HiOutlineMagnifyingGlass size={24} />
