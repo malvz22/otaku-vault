865fd3501763d3b632dfe2fa7d7c847ec3de417f
Malvin A <malvinaudriansyah77@gmail.com>
2025-01-21 19:25:44 +0700

    added validation to prevent page value reaching 0 or exceeding lastPage, and First and Last button to navigate to First Page and Last Page

--- src/app/components/Utilities/Pagination.tsx
+++ src/app/components/Utilities/Pagination.tsx
@@ -11,19 +11,37 @@ const Pagination: React.FC<PaginationProps> = ({ page, lastPage, setPage }) => {
       top: 0,
     });
   };
+
   const handleNextPage = () => {
+    if (page === lastPage) return;
     setPage((prevState) => prevState + 1);
     scrollTop();
   };
+  const handleLastPage = () => {
+    setPage(lastPage);
+    scrollTop();
+  };
+  const handleFirstPage = () => {
+    setPage(1);
+    scrollTop();
+  };
   const handlePrevPage = () => {
+    if (page === 1) return;
     setPage((prevState) => prevState - 1);
     scrollTop();
   };
+
   return (
     <div className="flex flex-row gap-3 justify-center items-center py-4 px-2 text-2xl">
       <button
         className="transition-all hover:text-[#1E90FF] duration-700"
-        onClick={handlePrevPage}
+        onClick={() => handleFirstPage()}
+      >
+        First
+      </button>
+      <button
+        className="transition-all hover:text-[#1E90FF] duration-700"
+        onClick={() => handlePrevPage()}
       >
         Prev
       </button>
@@ -35,6 +53,12 @@ const Pagination: React.FC<PaginationProps> = ({ page, lastPage, setPage }) => {
         onClick={() => handleNextPage()}
       >
         Next
+      </button>
+      <button
+        className="transition-all hover:text-[#1E90FF] duration-700"
+        onClick={() => handleLastPage()}
+      >
+        Last
       </button>
     </div>
   );
